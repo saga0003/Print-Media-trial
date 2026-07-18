20d5ca42449e65204fc39eef0e7b891951bd61ae
saga0003 <sagar@smis.edu.in>
2026-07-18 09:32:40 +0530

    Add V8 stage analytics page

---
+++ SalesOS-V8-Stage-Analytics/payload/src/app/stage-analytics/page.tsx
@@ -0,0 +1,7 @@
+import StageAnalyticsV8 from '@/components/salesos/views/stage-analytics-v8';
+
+export const dynamic = 'force-dynamic';
+
+export default function StageAnalyticsPage() {
+  return <StageAnalyticsV8 />;
+}
